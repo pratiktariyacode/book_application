efb4c85b4d008ced7098ae9b6c63cba09ff3ff1f
pratiktariya <pratiktariya000@gmail.com>
2025-06-28 20:24:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from tkinter import *
-from tkinter import messagebox
+import tkinter as tk 
+from tkinter import messagebox, ttk
+
+
 
 def command_page():
     print("Loging page")
 
 
 def admin_page_fun():
+    root.destroy()
     admin_page = Tk()
     admin_page.title("admin page")
     admin_page.minsize(500,500)
     admin_page.minsize(500,500)
     admin_page.mainloop()
 
-def user_page_fun():
-    root.destroy()
-    user_page = Tk()
-    user_page.title("user page book store")
-    Label(user_page,text="user book store").grid(row=0,column=1)
-    user_page.minsize(500,500)
-    user_page.maxsize(500,500)
-    user_page.mainloop()
     
 
 root = Tk()
@@ -35,10 +31,43 @@ space = Label(root,pady=2).grid(row=3,column=1)
 
 
 
+
+def on_tree_select(self, event):
+    selected = self.tree.selection()
+    if selected:
+        self.selected_book_id = int(selected[0])
+    else:
+        self.selected_book_id = None
+
+def user_loging():
+        print("main page")
+
+
+
+def user_page_fun():
+    root.destroy()
+    user_page = Tk()
+    user_page.title("book store free Download")
+    Label(user_page, text='Book List', font=('Arial', 16)).pack(pady=10)
+    tree = ttk.Treeview(user_page, columns=('Title', 'Author','Name'), show='headings')
+    tree.heading('Title', text='Title')
+    tree.heading('Author', text='Author')
+    tree.heading('Name',text='name')
+    tree.pack(fill=tk.BOTH, expand=True, padx=10, pady=5)
+    tree.bind('<<TreeviewSelect>>', on_tree_select)
+    btn1 = Button(user_page,text="View").pack()
+    btn2 = Button(user_page,text="Download").pack() 
+    loggaupt = Button(user_page,text="Logaut",command=user_loging).pack() 
+    user_page.minsize(500,500)
+    user_page.maxsize(500,500)
+    user_page.mainloop()
+
+
+
+
 def user_password():
     try:
         if e1.get() == "admin" and e2.get() == "admin":
-            root.destroy()
             admin_page_fun()
         elif e1.get() == "" and e2.get() == "":
             messagebox.showinfo('Info',"enter username and password")
@@ -54,16 +83,19 @@ def user_password():
 loging_btn = Button(root,text="Loging",command=user_password).grid(row=4,column=1)
 space = Label(root,pady=2).grid(row=5,column=1)
 user_btn = Button(root,text="Book store",command=user_page_fun).grid(row=6,column=1)
-
 root.minsize(500,500)
 root.maxsize(500,500)
 
 
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     root.mainloop()
     
+
+
+
+
+
+
 
 
 
